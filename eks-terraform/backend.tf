e0b38c58f6dbe0ec5cc330f7347e9ebec5edd105
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-05-12 20:59:18 +0530

    Create backend.tf

---
+++ eks-terraform/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">= 5.25.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "state-remote-store"
+    key    = "eks/terraform.tfstate"
+    region = "ap-south-1"
+  }
+
+  required_version = ">= 1.6.3"
+}
