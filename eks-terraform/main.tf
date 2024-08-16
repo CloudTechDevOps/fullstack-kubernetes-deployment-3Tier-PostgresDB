50144afb06535470b0794d0713b4ec3a730cda6d
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-08-16 16:52:57 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"  # Specify your desired region
+  region = "us-east-1"  # Specify your desired region
 }
 
  #Creating IAM role for EKS
@@ -121,7 +121,7 @@ data "aws_subnet" "subnet-1" {
  vpc_id = data.aws_vpc.main.id
  filter {
     name = "tag:Name"
-    values = ["MyPublicSubnet01"]
+    values = ["Jumphost-subnet1"]
  }
 }
 
@@ -129,14 +129,14 @@ data "aws_subnet" "subnet-2" {
  vpc_id = data.aws_vpc.main.id
  filter {
     name = "tag:Name"
-    values = ["MyPublicSubnet02"]
+    values = ["Jumphost-subnet2"]
  }
 }
 data "aws_security_group" "selected" {
   vpc_id = data.aws_vpc.main.id
   filter {
     name = "tag:Name"
-    values = ["devops-project-veera"]
+    values = ["Jumphost-sg"]
  }
 }
 
